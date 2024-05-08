cfccd1c8d76d699f11fec204f57d3e8231e35d54
user <user@DESKTOP-FEA4IDF>
2024-05-08 13:06:31 +0900

    1111

---
+++ week3_sum(control-array-classmeMemberVariable)/src/sum/classMember__sum.java
@@ -0,0 +1,10 @@
+package sum;
+
+public class classMember__sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ week3_sum(control-array-classmeMemberVariable)/src/sum/method_sum.java
@@ -0,0 +1,10 @@
+package sum;
+
+public class method_sum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
