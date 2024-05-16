937aad279a0244ca8d04a82eb4784c549d516b26
user <user@DESKTOP-FEA4IDF>
2024-05-16 17:55:16 +0900

    1111

--- 자바미니프로젝트_화면UI/src/com/sist/client/MenuPanel.java
+++ 자바미니프로젝트_화면UI/src/com/sist/client/MenuPanel.java
@@ -1,20 +1,22 @@
 
 package com.sist.client;
-
-import java.awt.CardLayout;
-import java.awt.Color;
+import java.awt.GridLayout;
 
 import javax.swing.*;
-
-public class ControllerPanel extends JPanel {
-	public CardLayout card = new CardLayout();
-	public HomePanel hp = new HomePanel();
-	public FindPanel fp = new FindPanel();
-	public ControllerPanel() {
-		setLayout(card);	// 위에 오는 레이아웃이 먼저 보임.
-		add("HOME", hp);
-		add("FIND", fp);
-
-	}
-	
+// 홈 검색 채팅 게시판 뉴스 ...
+public class MenuPanel extends JPanel{
+    public JButton b1,b2,b3,b4,b5,b6;
+    // 초기화 => 배치 
+    public MenuPanel()
+    {
+    	setLayout(new GridLayout(1,5,5,5)); // 같은 크기의 버튼 생성 
+    	b1=new JButton("홈"); // 목록 / 상세보기 
+    	b2=new JButton("검색"); // 검색 / 상세보기  
+    	b3=new JButton("채팅"); // 채팅 / 쪽지보내기 
+    	b4=new JButton("게시판");//목록 , 상세보기 , 수정 , 추가 , 삭제 
+    	b5=new JButton("뉴스");// 뉴스 검색 
+    	b6=new JButton("나가기");
+    	// 추가 
+    	add(b1);add(b2);add(b3);add(b4);add(b5);add(b6);
+    }
 }
