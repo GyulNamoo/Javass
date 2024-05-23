414742da606e8f52e1b521e2ca9c75385006c375
user <user@DESKTOP-FEA4IDF>
2024-05-23 17:16:39 +0900

    1111

---
+++ Chapter10_Library(java.util)_02/src/com/sist/util/Collection_Sum_01.java
@@ -0,0 +1,37 @@
+package com.sist.util;
+/*
+ *    라이브러리
+ *      => 클래스 : 하는 역할
+ *         ----
+ *          => 메소드 : 원형 (리턴형/매개변수)
+ *             ---- 어떤 기능을 수행하는지 ...
+ *                  -----------
+ *                  프로그램에서 사용하는 위치
+ *    ---------------------------------
+ *     => 사용처 
+ *     ---------- 츠로그램 : 라이브러리 + 사용자 정의(메소드, 클래스)
+ *                       ---------  --------
+ *                          50%       50%
+ *                         ---------------
+ *     java.lang / java.util / java.text / java.io / java.net / java.sql
+ *     ---------   ---------   ---------   -------   --------   --------
+ *                                                    URL       Connection / Statement / ResultSet
+ *                                                    URLEncoder
+ *                                         FileInputStream / FileOutputStream
+ *                                         File
+ *                                         FileReader / FileWriter
+ *                                         BufferReader
+ *                              SimpleDataFormat / DecimalFormat
+ *                  Date / StringTokenizer / ArrayList / Map
+ *      Object / String / StringBuffer / Math / Wrapper 
+ *                                              Integer / Double / Boolean
+ *     -----------------------------------------------------------------
+ */
+public class Collection_Sum_01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
