391889ad12b0d1dda46c4d66985044f8f010bbd5
user <user@DESKTOP-FEA4IDF>
2024-05-27 17:51:58 +0900

    1111

---
+++ Chapter11_Library(java.io)_01/src/com/sist/io/Library_09.java
@@ -0,0 +1,34 @@
+package com.sist.io;
+// 파일 쓰기 => 문자 스트림 (2byte) => 한글 사용이 편리 
+// FileWriter 
+import java.io.*;
+public class Library_09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		FileWriter fw = null;
+		try
+		{
+				fw = new FileWriter("c:\\java_data\\board.txt",true); // 없는 경우에는 자동 생성 //true를 사용하지 않을 경우 덮어쓰기!!
+				// 폴더는 생성하지 않는다
+				String msg = "ABCDEFGHIJKLMNOOOOOOOOOOOOOOOO1322232313242348\r\n";
+				fw.write(msg);
+				System.out.println("저장완료!!");
+		}catch (Exception ex) 
+		{
+			
+		}finally
+		{
+			try
+			{
+				fw.close();
+			}
+			catch(Exception ex)
+			{
+				
+			}
+			
+		}
+	}
+
+}
