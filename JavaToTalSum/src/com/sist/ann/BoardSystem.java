3d00f4b6e20c298cf7344c7668913555a6654ca1
user <user@DESKTOP-FEA4IDF>
2024-05-29 17:47:22 +0900

    1111

---
+++ JavaToTalSum/src/com/sist/ann/BoardSystem.java
@@ -0,0 +1,30 @@
+package com.sist.ann;
+
+public class BoardSystem {
+	@RequestMapping("insert.jps")
+	public void boardInsert()
+	{
+		System.out.println("오라클에 데이터 추가");
+	}
+	@RequestMapping("update.jsp")
+	public void boardUpdate()
+	{
+		System.out.println("오라클에 데이터 수정");
+	}
+	@RequestMapping("delete.jsp")
+	public void boardDelete()
+	{
+		System.out.println("오라클에 데이서 삭제");
+	}
+	@RequestMapping("select.jsp")
+	public void boardSelect()
+	{
+		System.out.println("오라클에 데이터 읽기");
+	}
+	@RequestMapping("find.jsp")
+	public void boardFind()
+	{
+		System.out.println("오라클에 데이터 검색");
+	}
+
+}
